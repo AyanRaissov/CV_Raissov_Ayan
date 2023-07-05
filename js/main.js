c2b4ec2ddfac1a77b9cff756c3ee41b8ab162bf0
AyanRaissov <100411660+AyanRaissov@users.noreply.github.com>
2023-07-05 19:47:21 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,13 +3,7 @@ const navBtn = document.querySelector('#nav-btn');
 const navBtnImg = document.querySelector('#nav-btn-img');
 // const dataScroll = document.querySelectorAll('[data-scroll]');
 
-navBtn.onclick = () => {
-    if (nav.classList.toggle('open')) {
-        navBtnImg.src = "./img/icons/nav-close.svg";
-    } else {
-        navBtnImg.src = './img/icons/nav-open.svg';
-    }
-}
+
 
 AOS.init();
 
